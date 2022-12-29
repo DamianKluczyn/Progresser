c526ddd9e6f6a8677e8d145c5a8fef825418e23f
Damian Kluczyński <damiankluczynski00@gmail.com>
2022-12-29 19:56:18 +0100

    Changed directions, added routings

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('boards', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace src\controllers;
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController
+{
+
+    public function index()
+    {
+        //TODO dispaly login.html
+        die("index method");
+
+    }
+
+    public function register()
+    {
+        //TODO display register.html
+        die("register method");
+    }
+
+    public function boards()
+    {
+        //TODO display boards.html
+        die("boards method");
+    }
+}
