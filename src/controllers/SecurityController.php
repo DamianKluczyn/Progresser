b7f60eb69f07b5a6c07d2dc7de565ed8c91d4c9f
Damian Kluczyński <damiankluczynski00@gmail.com>
2022-12-29 22:46:05 +0100

    Updated login check

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -8,8 +8,16 @@ class SecurityController extends AppController
     public function login(){
         $user = new User('jnowak@pk.pl', 'nowak', 'admin');
 
-        var_dump($_POST);
-        die();
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        if($user->getLogin() !== $login){
+            return $this -> render('login',['messages' => ['User with this login not exist!']]);
+        }
+
+        if($user->getPassword() !== $password){
+            return $this -> render('password',['messages' => ['Wrong password']]);
+        }
     }
 
 }
