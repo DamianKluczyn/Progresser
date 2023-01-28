8a6ff4446aa0d0cce735119c93bbbb420efda498
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-29 00:42:41 +0100

    added file validation

--- src/controllers/BoardController.php
+++ src/controllers/BoardController.php
@@ -1,10 +1,38 @@
 <?php
 require_once 'AppController.php';
 
-class BoardController extends AppController
-{
-    public function addBoard(){
-        $this->render('add_board');
+class BoardController extends AppController {
+
+    const MAX_FILE_SIZE = 1024*1024;
+    const SUPPORTED_TYPES = ['image/png', 'image/jpeg'];
+    const UPLOAD_DIRECTORY = '/../public/uploads/';
+
+    private $messages = [];
+
+    public function addBoard() {
+        if($this->isPost() && is_uploaded_file($_FILES['file']['tmp_name']) && $this->validate($_FILES['file'])) {
+
+
+
+            return $this->render("boards", ['messages' => $this->messages]);
+        }
+
+        $this->render('add_board', ['messages' => $this->messages]);
+    }
+
+    private function validate(array $file): bool
+    {
+        if($file['size'] > self::MAX_FILE_SIZE) {
+            $this->messages[] = 'File is too large for destination file system';
+            return false;
+        }
+
+        if(!isset($file['type']) && !in_array($file['type'], self::SUPPORTED_TYPES)) {
+            $this->messages[] = 'File type is not supported';
+            return false;
+        }
+
+        return true;
     }
 
 }
