4a3f2c087fb44210edde38d7f280d12296f3e278
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-29 19:58:20 +0100

    fixed

--- config.php
+++ config.php
@@ -1,1 +1,7 @@
 <?php
+
+const USERNAME = 'yrivnkdnsqmsre';
+const PASSWORD = 'f8b12576b596d95aa061d8e3c2cd14bf046f651f67b00077483d8edaa484af0c';
+const HOST = 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com';
+const DATABSE = 'd2q163hlsvc4da';
+
